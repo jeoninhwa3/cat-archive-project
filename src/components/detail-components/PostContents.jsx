518eb20771b224fc8fe5a0db642145935751ee71
sobbingtofu <kukubasak@naver.com>
2024-06-07 02:30:11 +0900

    fix: 머지하면서 깨진 supabase 임포트 오류 해결

--- src/components/detail-components/PostContents.jsx
+++ src/components/detail-components/PostContents.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import supabase from '../supabaseClient';
+import supabase from '../../supabaseClient';
 import styled from 'styled-components';
 import defaultImg from '../../assets/defaultImg.jpg';
 
